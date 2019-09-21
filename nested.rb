602ea092fb941a097ffc859feae54021d516f40b
Jared S <jaredjames2020@gmail.com>
2019-09-21 23:00:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
      
      binding.pry
      
-programmer_hash[:dennis_ritchie][:known_for][:languages][0]
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
